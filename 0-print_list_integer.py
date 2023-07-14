ba42858894fddf3fd42f62abac525995cf54c23a
umumararungu <umcynthi@gmail.com>
2023-07-14 20:01:33 +0200

    changes

--- 0-print_list_integer.py
+++ 0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def first_list (my_list = [1,2,3,4,5]):
+def first_list (my_list = []):
     for num in my_list:
-        print("{}".format(num))
+        print([".format(", "{:d}"])
